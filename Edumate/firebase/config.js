8f0b7c0003e3056d26e93478a36802c529872f87
pamudithaKekulandara <pkekulandera@gmail.com>
2023-03-23 13:52:21 +0530

    firebase

--- Edumate/firebase/config.js
+++ Edumate/firebase/config.js
@@ -1,25 +1,24 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import {getFirestore} from "firebase/firestore";
+import { initializeApp } from 'firebase/app'
+import { getFirestore } from 'firebase/firestore'
+import { getStorage } from 'firebase/storage'
+import { getAnalytics } from 'firebase/analytics'
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCdyJfiGYdKgREA9sjrEKbar38pLiKq6cg",
-  authDomain: "edumate2023-d209f.firebaseapp.com",
-  projectId: "edumate2023-d209f",
-  storageBucket: "edumate2023-d209f.appspot.com",
-  messagingSenderId: "850360430796",
-  appId: "1:850360430796:web:6c6f8937718363bd2e5269",
-  measurementId: "G-W79HJ33DX6"
-};
+  apiKey: 'AIzaSyDqI0Rn7Ti9yz5ULnffRnnInas1uRZ5CNw',
+  authDomain: 'edumate-c06a6.firebaseapp.com',
+  projectId: 'edumate-c06a6',
+  storageBucket: 'edumate-c06a6.appspot.com',
+  messagingSenderId: '769885791348',
+  appId: '1:769885791348:web:35d39d9e74da94ee9f1d21',
+  measurementId: 'G-JWMVFCL8YG',
+}
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-
-//Initialize Firestore
-export const db = getFirestore(app);
+const app = initializeApp(firebaseConfig)
+export const db = getFirestore(app)
+export const storage = getStorage(app)
