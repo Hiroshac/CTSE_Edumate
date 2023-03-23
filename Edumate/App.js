e4d354c2731f02f0a6c237e03c17ad442a435dee
pamudithaKekulandara <pkekulandera@gmail.com>
2023-03-23 18:34:00 +0530

    error fixing

--- Edumate/App.js
+++ Edumate/App.js
@@ -1,8 +1,6 @@
 import React from 'react'
 import { RootStack } from './src/components/RootStack'
 import { TeacherDash } from './src/screens/Teacher/TeacherDash'
-import { UploadLink } from './src/screens/Teacher/UploadLink'
-import { UploadNote } from './src/screens/Teacher/UploadNote'
 // import { StudentSubject } from './src/screens/student/StudentSubject'
 // import { StudentNotes } from './src/screens/student/StudentNotes'
 // import { StudentFeedback } from './src/screens/student/StudentFeedback'
@@ -23,31 +21,8 @@ export default function App() {
     // <StudentFeedback/>
     // <StudentComment/>
     // <StudentExamTimeTable/>
-    // <RootStack /> 
-    // <StduentAnswerSheetUpload/> 
-    // <UploadLink/>
-    // <UploadNote/>
-    <TeacherDash/>
+    <TeacherDash />
+    // <StduentAnswerSheetUpload/>
     // <Upload/>
   )
-  const Stack = createNativeStackNavigator();
-  return (
-    <View style={styles.container}>
-      <Stack.Navigator>
-      <Stack.Screen name="Home" component={Home} />
-    </Stack.Navigator>
-      <Text>Edumate Application Join for develop the new world</Text>
-      <Text>Wellcome to new world</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
